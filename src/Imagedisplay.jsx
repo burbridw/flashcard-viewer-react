dca0fd8a3d3c3d77be363e9b8ebabcb2cee5acd0
burbridw <111381782+burbridw@users.noreply.github.com>
2023-07-12 05:52:12 +0900

    dev july 11th

--- src/Imagedisplay.jsx
+++ src/Imagedisplay.jsx
@@ -1,23 +1,30 @@
 import { Imgelement } from "./Imgelement"
 import selectObj from "./selectobj.json"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 
 export function Imagedisplay() {
-    const [imageIndex, setImageIndex] = useState(0)
     const [listIndex, setListIndex] = useState(0)
     const [imageList, setImageList] = useState(selectObj[Object.keys(selectObj)[listIndex]])
+    const [listLength, setListlength] = useState(imageList.length)
+    const [imageIndex, setImageIndex] = useState(0)
 
-    // const listLength = imageList.length
-    // const listTitles = Object.keys(selectObj)
+    const listTitles = Object.keys(selectObj)
     // const titlesLength = listTitles.length
-    
 
-    // if (imageIndex < 0 ) {
-    //     setImageIndex(0)
-    // }
-    // if ( imageIndex === listLength) {
-    //     setImageIndex(listLength - 1)
-    // } 
+    if (imageIndex < 0 ) {
+        setImageIndex(0)
+    }
+    if ( imageIndex === listLength) {
+        setImageIndex(listLength - 1)
+    } 
+
+    useEffect(()=>{
+        setImageList(selectObj[Object.keys(selectObj)[listIndex]])
+        setImageIndex(0)
+    }, [listIndex])
+    useEffect(()=>{
+        setListlength(imageList.length)
+    }, [imageList])
 
     return (
         <>
@@ -26,15 +33,15 @@ export function Imagedisplay() {
                     BACK
                 </div>
                 <div className="image-display-main">
-                    {/* <img src={imageList[imageIndex]} id="display"/> */}
-                    <Imgelement list={imageList} index={imageIndex} />
+                    <img src={imageList[imageIndex]} id="display"/>
+                    {/* <Imgelement list={imageList} index={imageIndex} changeImage={setImage}/> */}
                 </div>
                 <div className="image-display-button" id="image-next" onClick={() => setImageIndex(currentIndex => currentIndex + 1)}>
                     NEXT
                 </div>
             </div>
             <div className="change-button-box">
-                <div className="change-button">
+                <div className="change-button" onClick={() => setListIndex(currentIndex => currentIndex + 1)}>
                     CHANGE
                 </div>
             </div>
--- src/Imgelement.jsx
+++ src/Imgelement.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 
-export function Imgelement({list, index}) {
+export function Imgelement({list, index, changeImage}) {
     const [image, setImage] = useState(list[index])
     return (
         <img id="display" src={image} />
