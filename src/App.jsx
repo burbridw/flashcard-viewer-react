86b8e70b6d6c40c14d88ed6d7f6ffe9e623a73f9
burbridw <111381782+burbridw@users.noreply.github.com>
2023-07-12 15:50:47 +0900

    dev july 12th

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { ControlBar } from "./ControlBar"
 import { Greencontrols } from "./Greencontrols"
 import "./styles.css"
 import { Imagedisplay } from "./Imagedisplay"
+import { Topicbuttons } from "./Topicbuttons"
 
 function App() {
   const [goOrange, setGoOrange] = useState(true)
   const [orangeState, setOrangeState] = useState(()=> {
     return <ControlBar />
   })
+  const [btnPush, setBtnPush] = useState("")
 
   function changeState() {
     if (goOrange) {
@@ -23,15 +25,21 @@ function App() {
       })
     }
   } 
+
+  function acceptButtonPush(button) {
+    setBtnPush(button)
+  }
+
   return (
     <div className="background">
-      {orangeState}
+    <Topicbuttons sendBtnInfo={acceptButtonPush}/>
+      {/* {orangeState}
       <div className="change-button-box">
           <div className="change-button" onClick={changeState}>
             CHANGE
           </div>
-      </div>
-      <Imagedisplay />
+      </div> */}
+      <Imagedisplay button={btnPush}/>
     </div>
   )
 }
